633450c94989022d831a1be4cf8cf9ba5e7b5e1e
Sebastian Himbert <63553758+himberts@users.noreply.github.com>
2022-04-01 08:56:08 -0400

    Update mxray.py

--- bin/mxray.py
+++ bin/mxray.py
@@ -47,11 +47,50 @@ if __name__=='__main__':
     output["B"] = "%1.2f" % 5.23
     message.udpmessage({"Running Calculation"})
 
+    output['plotline'] = {
+            "data" : [
+                    {
+                            "x": [1, 2, 3, 4],
+                            "y": [10, 15, 13, 17],
+                            "mode": "markers",
+                            "marker": {
+                                    "color": "rgb(219, 64, 82)",
+                                    "size": 12
+                            }
+                    },
+                    {
+                            "x" : [2, 3, 4, 5],
+                            "y" : [16, 5, 11, 9],
+                            "mode" : "lines",
+                            "line" : {
+                                    "color" : "rgb(55, 128, 191)",
+                                    "width": 3
+                            }
+                    },
+                    {
+                            "x" : [1, 2, 3, 4],
+                            "y" : [12, 9, 15, 12],
+                            "mode" : "lines+markers",
+                            "marker" : {
+                                    "color" : "rgb(128, 0, 128)",
+                                    "size": 8
+                            },
+                            "line" : {
+                                    "color" : "rgb(128, 0, 128)",
+                                    "width" : 1
+                            }
+                    }
+            ],
+            "layout" : {
+                    "title" : "Line and Scatter Styling"
+            }
+    }
+
+    output['_textarea'] = "JSON output from executable:\n" + json.dumps( output, indent=4 ) + "\n\n";
+    output['_textarea'] += "JSON input to executable:\n" + json.dumps( json_variables, indent=4 ) + "\n";
 
 
 
-
-    
     print( json.dumps(output) ) # convert dictionary to json and output
 
     # ## read model parameters
