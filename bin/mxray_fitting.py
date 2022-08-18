8a94f8d96dbe1777fc77f65faef0514139768d8e
Sebastian Himbert <63553758+himberts@users.noreply.github.com>
2022-08-18 19:06:58 -0400

    Update Fitting Tab and Fit Backend

--- bin/mxray_fitting.py
+++ bin/mxray_fitting.py
@@ -30,13 +30,13 @@ if __name__=='__main__':
     xi = float(json_variables['xi'])
     eta = float(json_variables['eta'])
     q = float(json_variables['q']) # number of points in (simulated) q
-#    qparcutstext = json_variables['qparcuts']
-    SampleDetectorDistance = float(json_variables['sd'])
-    PixelSize = float(json_variables['pxs']) # number of points in p(r)
-    XOrigin = float(json_variables['xorigin'])
-    YOrigin = float(json_variables['yorigin'])
-    WaveLength = float(json_variables['lambda'])
-    DataFile = json_variables['data']
+# #    qparcutstext = json_variables['qparcuts']
+#     SampleDetectorDistance = float(json_variables['sd'])
+#     PixelSize = float(json_variables['pxs']) # number of points in p(r)
+#     XOrigin = float(json_variables['xorigin'])
+#     YOrigin = float(json_variables['yorigin'])
+#     WaveLength = float(json_variables['lambda'])
+#     DataFile = json_variables['data']
     folder = json_variables['_base_directory'] # output folder dir
 
 
@@ -58,14 +58,14 @@ if __name__=='__main__':
     sock.sendto(doc_string.encode(),(UDP_IP,UDP_PORT))
 
 
-    a = xray(filename=str(DataFile[0]), px=0.1, sampledetectdist=SampleDetectorDistance, xorigin=XOrigin, yorigin=YOrigin, wavelength=WaveLength, q1='0.0945')
+    # a = xray(filename='outputfile.dat', px=0.1, sampledetectdist=SampleDetectorDistance, xorigin=XOrigin, yorigin=YOrigin, wavelength=WaveLength, q1='0.0945')
 #    qparcuts = np.char.split(qparcutstext, ',').astype(np.float32)
 
     #a = xray(filename=str(DataFile[0]), px=PixelSize, sampledetectdist=332.1269, xorigin=410.983, yorigin=0.5101, wavelength=1.541867)
     # a.plottiff()
-    a.cropimg(point1=0.3, point2=0.35, plot=0)
-    a.calcmeanandplot(plot=0)
-    a.export()
+    # a.cropimg(point1=0.3, point2=0.35, plot=0)
+    # a.calcmeanandplot(plot=0)
+    # a.export()
 
     s = subprocess.check_output(["mxray_bending","-m","fitd", "-z", str(xi),"-e" ,str(eta), "-f", "outputfile.dat","--Lr","300","--sr","100","-o","TestFit"])
 
