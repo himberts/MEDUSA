9595a15ffb9914ed393158918cddb55b79164f88
Sebastian Himbert <63553758+himberts@users.noreply.github.com>
2022-08-19 10:40:34 -0400

    update Progressbar in Reduction

--- bin/mxray_reduction.py
+++ bin/mxray_reduction.py
@@ -38,14 +38,30 @@ if __name__=='__main__':
 
     # message = genapp(json_variables)
     output = {} # create an empty python dictionary
+    UDP_IP = json_variables['_udphost']
+    UDP_PORT = int( json_variables['_udpport'] )
+    sock = socket.socket(socket.AF_INET, # Internet
+            socket.SOCK_DGRAM) # UDP
+
+    socket_dict={}
+    socket_dict['_uuid'] = json_variables['_uuid']
+
+    socket_dict['_textarea'] = "Process started ..."
+    socket_dict['progressbar'] = 0
+    # socket_dict['progress_html'] = '<center>'+svalue+'</center>'
+    doc_string = json.dumps(socket_dict)
+    sock.sendto(doc_string.encode(),(UDP_IP,UDP_PORT))
+
+
 
     a = xray(filename=str(DataFile[0]), px=0.1, sampledetectdist=SampleDetectorDistance, xorigin=XOrigin, yorigin=YOrigin, wavelength=WaveLength, q1='0.0945')
-    # qparcuts = np.char.split(qparcutstext, ',').astype(np.float32)
-    #a = xray(filename=str(DataFile[0]), px=PixelSize, sampledetectdist=332.1269, xorigin=410.983, yorigin=0.5101, wavelength=1.541867)
-    # a.plottiff()
-    # content = ''
-    # for file in os.listdir(folder):
-    #     content = "%s \n %s" % (content,file)
+
+    socket_dict['_textarea'] = "Data Loaded ..."
+    socket_dict['progressbar'] = 0.2
+    # socket_dict['progress_html'] = '<center>'+svalue+'</center>'
+    doc_string = json.dumps(socket_dict)
+    sock.sendto(doc_string.encode(),(UDP_IP,UDP_PORT))
+
     qparCutsList  = qparcutstext.split(',')
     qparCuts = np.ndarray(len(qparCutsList))
     for k in range(len(qparCutsList)):
@@ -53,10 +69,34 @@ if __name__=='__main__':
         # content = "%s \n %f" % (content,qparCuts[k])
 
     a.plottiff(show=0)
+    socket_dict['_textarea'] = "2D Graphics created ..."
+    socket_dict['progressbar'] = 0.4
+    # socket_dict['progress_html'] = '<center>'+svalue+'</center>'
+    doc_string = json.dumps(socket_dict)
+    sock.sendto(doc_string.encode(),(UDP_IP,UDP_PORT))
     a.cropimg(qparCuts, showcrop=1)
     # a.cropimg(point1=0.3, point2=0.35, plot=0)
+    socket_dict['_textarea'] = "qr cuts created ..."
+    socket_dict['progressbar'] = 0.6
+    # socket_dict['progress_html'] = '<center>'+svalue+'</center>'
+    doc_string = json.dumps(socket_dict)
+    sock.sendto(doc_string.encode(),(UDP_IP,UDP_PORT))
+
     a.calcmeanandplot(show=0, showopt=0)
+
+    socket_dict['_textarea'] = "Cuts averaged ..."
+    socket_dict['progressbar'] = 0.8
+    # socket_dict['progress_html'] = '<center>'+svalue+'</center>'
+    doc_string = json.dumps(socket_dict)
+    sock.sendto(doc_string.encode(),(UDP_IP,UDP_PORT))
+
     a.export()
+
+    socket_dict['_textarea'] = "Data exported ..."
+    socket_dict['progressbar'] = 1
+    # socket_dict['progress_html'] = '<center>'+svalue+'</center>'
+    doc_string = json.dumps(socket_dict)
+    sock.sendto(doc_string.encode(),(UDP_IP,UDP_PORT))
     #
     #
     output["reddat"] = "%s/outputfile.dat" % folder
