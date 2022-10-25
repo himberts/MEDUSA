7631440d5f5ffb5272870a42f17c5ae020b1e6db
Sebastian Himbert <himberts@mcmaster.ca>
2022-10-25 15:13:32 -0400

    Update mxray_fitting.py

--- bin/mxray_fitting.py
+++ bin/mxray_fitting.py
@@ -115,7 +115,7 @@ if __name__=='__main__':
                             "title": "Iteration (a.u.)"
                         },
                         "yaxis": {
-                            "title": u"&#8491"
+                            "title": "\\Chi"
                         }
                         # "xaxis" : "Iteration (a.u.)",
                         # "yaxis" : "Xisquare"
